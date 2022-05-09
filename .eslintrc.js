8999d5b746321da889f355a9343dcbbcc228dae6
David Ruisinger <david.ruisinger@gmail.com>
2022-05-09 15:24:32 +0200

    feat: add ts linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,42 @@
 module.exports = {
-  extends: ["next", "prettier"],
-  plugins: ["simple-import-sort", "sort-destructure-keys"],
+  extends: ['plugin:@typescript-eslint/recommended', 'next', 'prettier'],
+  plugins: ['simple-import-sort', 'sort-destructure-keys'],
   rules: {
-    "no-console": [
-      "error",
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
       {
-        allow: ["info", "error"],
+        functions: false,
+        typedefs: false,
       },
     ],
-    "no-unused-vars": ["error"],
-    "react-hooks/exhaustive-deps": "warn",
-    "react/jsx-sort-props": [
-      "error",
+    'no-console': [
+      'error',
+      {
+        allow: ['info', 'error'],
+      },
+    ],
+    'no-unused-vars': ['error'],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-sort-props': [
+      'error',
       {
         ignoreCase: false,
       },
     ],
-    "react/prop-types": "off",
-    "simple-import-sort/exports": "error",
-    "simple-import-sort/imports": "error",
-    "sort-destructure-keys/sort-destructure-keys": "error",
-    "sort-keys": [
-      "error",
-      "asc",
+    'react/prop-types': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-destructure-keys/sort-destructure-keys': 'error',
+    'sort-keys': [
+      'error',
+      'asc',
       {
         caseSensitive: false,
         minKeys: 2,
         natural: false,
       },
     ],
-    "sort-vars": "error",
+    'sort-vars': 'error',
   },
-};
+}
