c9835f3fed942bda615ddd1d55bb23fcc8ced428
Timo Müller <timo.h.mueller@gmail.com>
2022-05-12 14:02:26 +0300

    add simple list of highlighted actions

---
+++ components/ActionsCarousel/index.tsx
@@ -0,0 +1,56 @@
+require('./styles.less')
+
+import React from 'react'
+import { Card, Carousel, Tag } from 'antd'
+import { MOBILE_BREAKPOINT } from '../../utils'
+import { LogoGroup } from '../LogoGroup'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons'
+
+const FAKE_LOGOS = [
+    { logoUrl: 'https://via.placeholder.com/150' },
+    { logoUrl: 'https://via.placeholder.com/150' },
+    { logoUrl: 'https://via.placeholder.com/150' },
+    { logoUrl: 'https://via.placeholder.com/150' },
+]
+
+
+export const ActionsCarousel = ({ actions, onSelect }: { actions: any, onSelect?: any }) => {
+
+    const responsiveConfig = [
+        {
+            breakpoint: MOBILE_BREAKPOINT,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+
+    return (
+        <Carousel
+            prevArrow={<ArrowLeftOutlined />}
+            nextArrow={<ArrowRightOutlined />}
+            className='actions-carousel'
+            dots={false}
+            responsive={responsiveConfig}
+            slidesToScroll={3}
+            slidesToShow={3}
+        >
+            {
+                actions.map((action: any, i: any) => {
+                    return (
+                        <Card key={`action-${i}`}>
+                            <Tag>Required</Tag>
+                            <div className='action-card-content'>
+                                <div className='action-card-title'>
+                                    {action.title}
+                                </div>
+                                <LogoGroup data={FAKE_LOGOS} label={'doing this'} size="small" />
+                            </div>
+                        </Card>
+                    )
+                })
+            }
+        </Carousel >
+    )
+}
--- pages/actions/index.tsx
+++ pages/actions/index.tsx
@@ -4,16 +4,20 @@ import type { GetStaticProps, NextPage } from 'next'
 import Logout from '../../components/Auth/Logout'
 import { SiderLayout, Main, Section, Sider } from '../../components/Layout'
 import { ActionsList } from '../../components/ActionsList'
+import { ActionsCarousel } from '../../components/ActionsCarousel'
 import { fetchAllActions } from '../../services/contentful'
 import { NAV } from './data'
 
 
 const Home: NextPage = (props: any) => {
   const { byTags } = props.actions
+  const highlightedActions = byTags['Tech'] // @TODO: replace with recommended, required, expired
+
   return (
     <SiderLayout nav={NAV}>
       <Main>
         <Section title='Dashboard' titleSize='big'>
+          <ActionsCarousel actions={highlightedActions} />
           <ActionsList actionsByTags={byTags} />
           <Logout />
         </Section>
