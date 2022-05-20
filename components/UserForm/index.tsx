dbe19f8dcdacc021d258dc4506eb505c468677d7
Timo Müller <timo.h.mueller@gmail.com>
2022-05-20 14:30:30 +0300

    adjust user form

--- components/UserForm/index.tsx
+++ components/UserForm/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Select } from 'antd'
+import { Button, Form, Input, InputNumber, Select } from 'antd'
 
 import { Country } from '../../services/contentful'
 import {
@@ -7,6 +7,8 @@ import {
   UserFragment,
 } from '../../services/lfca-backend'
 import { ROLES } from '../../utils'
+import { CLOUDINARY_PRESETS } from '../FileUpload/helper'
+import { ImageUpload } from '../FileUpload/ImageUpload'
 
 const { Option } = Select
 
@@ -63,11 +65,25 @@ export const UserForm = ({
         </Select>
       </Form.Item>
       <Form.Item
+        label="Company Id"
+        name="companyId"
+        rules={[{ message: 'Please add a companyId', required: false }]}
+      >
+        <Input placeholder="-Mdas211masud" />
+      </Form.Item>
+      <Form.Item
         label="Email"
         name="email"
         rules={[{ message: 'Please add an email', required: true }]}
       >
         <Input placeholder="greta@thunbergvc.earth" type="email" />
+      </Form.Item>
+      <Form.Item
+        label="Phone"
+        name="phone"
+        rules={[{ message: 'Please add a phone number', required: false }]}
+      >
+        <Input placeholder="+49 160 1234567" />
       </Form.Item>
       <Form.Item
         label="First name"
@@ -84,6 +100,13 @@ export const UserForm = ({
         <Input placeholder="Thunberg" />
       </Form.Item>
       <Form.Item
+        label="Picture"
+        name="picture"
+        rules={[{ message: 'Please add a picture', required: false }]}
+      >
+        <ImageUpload customPreset={CLOUDINARY_PRESETS.profilePictures} />
+      </Form.Item>
+      <Form.Item
         label="Roles"
         name="roles"
         rules={[{ message: 'Please select at least one role', required: true }]}
@@ -95,6 +118,13 @@ export const UserForm = ({
             </Option>
           ))}
         </Select>
+      </Form.Item>
+      <Form.Item
+        label="Sort weight"
+        name="sortWeight"
+        rules={[{ message: 'Please add sort weight', required: false }]}
+      >
+        <InputNumber max={100} min={0} placeholder={'50'} />
       </Form.Item>
       <Form.Item>
         <Button htmlType="submit" loading={isLoading} type="primary">
