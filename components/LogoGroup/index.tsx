5ea70de39486ed1410d814e350fde53ff5ba5fdc
Timo Müller <timo.h.mueller@gmail.com>
2022-05-23 20:13:16 +0300

    show empty state

--- components/LogoGroup/index.tsx
+++ components/LogoGroup/index.tsx
@@ -1,6 +1,5 @@
 require('./styles.less')
 
-import { PlusOutlined } from '@ant-design/icons'
 import { Avatar, AvatarProps } from 'antd'
 import classNames from 'classnames'
 
@@ -13,13 +12,22 @@ interface LogoGroupProps {
   size: AvatarProps['size']
 }
 
-export const LogoGroup = ({ data, label, reverse, size }: LogoGroupProps) => {
+export const LogoGroup = ({
+  data = [],
+  label,
+  reverse,
+  size,
+}: LogoGroupProps) => {
   return (
     <div className={classNames('logo-group', size, { reverse: reverse })}>
       <Avatar.Group size={size}>
-        {data?.map((item, i) => (
-          <Avatar key={`avatar-${i}`} src={item.logoUrl} />
-        )) || <Avatar className="empty" icon={<PlusOutlined />} />}
+        {data?.length > 0 ? (
+          data?.map((item, i) => (
+            <Avatar key={`avatar-${i}`} src={item.logoUrl} />
+          ))
+        ) : (
+          <Avatar className="empty">-</Avatar>
+        )}
       </Avatar.Group>
       {label && <div className="label">{label}</div>}
     </div>
