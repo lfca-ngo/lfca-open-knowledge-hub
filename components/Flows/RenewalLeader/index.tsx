3938cec478b397291b00a319cade5f65c7936067
Timo Müller <timo.h.mueller@gmail.com>
2022-05-25 18:06:33 +0300

    add renewal flow

---
+++ components/Flows/RenewalLeader/index.tsx
@@ -0,0 +1,137 @@
+import { Button, Drawer, Tag } from 'antd'
+import { useState } from 'react'
+
+import { useUser } from '../../../hooks/user'
+import { PersonalCarbonCalculator } from '../../../tools/PersonalCarbonCalculator'
+import { Pledge } from '../../Pledge'
+
+interface StepProps {
+  onNext: () => void
+}
+
+const Intro = ({ onNext }: StepProps) => {
+  const { user } = useUser()
+
+  return (
+    <div>
+      <Tag className="super-text">Pledge</Tag>
+      <h1>{`Hi ${user?.firstName}, time to renew your pledge!`}</h1>
+      <p>
+        {`We started LFCA with the goal to accelerate the transition towards a
+        sustainable economy. To make this happen, we need to leverage our
+        influence on a personal, business and political level. Please start by
+        signing our Green Pledge as a leader of your company.`}
+      </p>
+      <Button onClick={onNext} size="large" type="primary">
+        Continue
+      </Button>
+    </div>
+  )
+}
+
+interface FootprintProps extends StepProps {
+  questionnaire: any
+}
+
+const Footprint = ({ onNext, questionnaire }: FootprintProps) => {
+  const [drawerVisible, setDrawerVisible] = useState(false)
+
+  const saveAndContinue = () => {
+    // @TODO: save to database
+    // show loading spinner
+    setDrawerVisible(false)
+    onNext()
+  }
+
+  return (
+    <div>
+      <Tag className="super-text">Intro</Tag>
+      <h1>{`Great! Change starts with yourself. Let's start by examining your personal footprint`}</h1>
+      <p>
+        {`To understand the issue of climate change, it's important to realize where we cause emissions in our day-to-day lifes and to learn how we can reduce our output of carbon.`}
+      </p>
+      <Button
+        onClick={() => setDrawerVisible(true)}
+        size="large"
+        type="primary"
+      >
+        Calculate my footprint
+      </Button>
+
+      <Drawer
+        className="fullscreen-drawer-bottom"
+        height={'100%'}
+        onClose={() => setDrawerVisible(false)}
+        placement="bottom"
+        visible={drawerVisible}
+      >
+        <PersonalCarbonCalculator
+          questionnaire={questionnaire}
+          saveResult={saveAndContinue}
+        />
+      </Drawer>
+    </div>
+  )
+}
+
+const Compare = ({ onNext }: StepProps) => {
+  const { user } = useUser()
+
+  return (
+    <div>
+      <Tag className="super-text">Compare</Tag>
+      <h1>{`Let's see how you compare to last year`}</h1>
+      <p>{`...`}</p>
+      <Button onClick={onNext} size="large" type="primary">
+        Continue
+      </Button>
+    </div>
+  )
+}
+
+const Share = ({ onNext }: StepProps) => {
+  return (
+    <div>
+      <Tag className="super-text">Share</Tag>
+      <h1>
+        {`Great job, you are now part of our community! Share these news and help
+        us spread the word`}
+      </h1>
+      <p>
+        {`Did you know? LFCA has grown entirely by word of mouth. As a non-profit
+        organization we rely on our community to increase our impact!`}
+      </p>
+
+      <div style={{ background: 'black', height: '100px' }}>
+        SHARING FEATURE
+      </div>
+
+      <Button onClick={onNext} size="large" type="primary">
+        Show Dashboard
+      </Button>
+    </div>
+  )
+}
+
+export const RenewalLeaderSteps = [
+  {
+    component: Intro,
+    description: 'Intro',
+    title: 'Renewal',
+  },
+  {
+    component: Footprint,
+    description: 'Understand your emissions',
+    title: 'Footprint',
+  },
+  {
+    component: Compare,
+    description: 'Compare to last year',
+    title: 'Compare',
+  },
+  {
+    component: Share,
+    description: 'Use your influence',
+    title: 'Share the news',
+  },
+]
---
+++ pages/renewal-leader.tsx
@@ -0,0 +1,53 @@
+import type { GetStaticProps, NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+import { RenewalLeaderSteps } from '../components/Flows'
+import { StepsLayout } from '../components/Layout'
+import { useUser } from '../hooks/user'
+import { fetchAllQuestionnaires } from '../services/contentful'
+import { withAuth } from '../utils/with-auth'
+
+const OnboardingLeader: NextPage = (props: any) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const router = useRouter()
+  const { user } = useUser()
+
+  const handleOnNext = () => {
+    if (currentStepIndex === RenewalLeaderSteps.length - 1) {
+      router.push('/')
+    } else {
+      setCurrentStepIndex((i) => i + 1)
+    }
+  }
+
+  const Step = RenewalLeaderSteps[currentStepIndex]?.component
+
+  return (
+    <StepsLayout
+      canClose
+      currentStepIndex={currentStepIndex}
+      onClose={() => router.push('/')}
+      steps={RenewalLeaderSteps}
+    >
+      {Step ? (
+        <Step
+          onNext={handleOnNext}
+          questionnaire={props?.questionnaires[user?.country || 'eu-DE']}
+        />
+      ) : null}
+    </StepsLayout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const questionnaires = await fetchAllQuestionnaires()
+
+  return {
+    props: {
+      questionnaires,
+    },
+  }
+}
+
+export default withAuth(OnboardingLeader)
