1f90efb5aa846d05b77f3ce05dd607e6a5446234
Timo Müller <timo.h.mueller@gmail.com>
2022-05-30 10:29:15 +0300

    move tracking to app hook

--- hooks/app.tsx
+++ hooks/app.tsx
@@ -1,8 +1,13 @@
+import { useRouter } from 'next/router'
 import React, { createContext, useContext, useEffect, useState } from 'react'
 
+import { INITIAL_VALUES, LS_ACTION_LIST } from '../components/ActionsList'
+import { trackEvent } from '../services/analytics'
 import { DESKTOP, getScreenSizeType } from '../utils'
+import { ACTIONS } from '../utils/routes'
 import { useDarkMode } from './useDarkMode'
 import { useLocalStorage } from './useLocalStorage'
+import { useScrollPosition } from './useScrollPosition'
 
 const CLIENT = 'client'
 const SERVER = 'server'
@@ -17,11 +22,16 @@ const initialState = {
 const AppContext = createContext(initialState)
 
 export const AppProvider = ({ children }: { children: any }) => {
+  const router = useRouter()
+  // states
   const [screenSize, setScreenSize] = useLocalStorage(
     SCREEN_SIZE,
     initialState.screenSize
   )
   const [isClient, setClient] = useState(initialState.isClient)
+  // reset position
+  const { resetPosition } = useScrollPosition(LS_ACTION_LIST, false)
+
   const key = isClient ? CLIENT : SERVER
 
   // set dark mode
@@ -33,7 +43,34 @@ export const AppProvider = ({ children }: { children: any }) => {
     const screenSize = getScreenSizeType(window, document)
     setClient(true)
     setScreenSize(screenSize)
+    // track page view
+    trackEvent({ name: 'pageView' })
   }, [])
+
+  // on route change
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // only persist the browsing state when the user goes from
+      // the dashboard to the action detail page and back
+      // in all other cases, reset the browsing state
+      const fromDashboardToDetails =
+        router.pathname === ACTIONS && url.startsWith('/action/')
+      const fromDetailPageToDashboard =
+        router.pathname.startsWith('/action/') && url === ACTIONS
+
+      if (!(fromDashboardToDetails || fromDetailPageToDashboard)) {
+        resetPosition(INITIAL_VALUES)
+      }
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router.pathname, resetPosition, router.events])
 
   // wait with initial render until client side
   // to avoid SSR flashing
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,47 +1,12 @@
 require('../styles/global.less')
 
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
-import { INITIAL_VALUES, LS_ACTION_LIST } from '../components/ActionsList'
 import { AppProvider } from '../hooks/app'
-import { useScrollPosition } from '../hooks/useScrollPosition'
-import { trackEvent } from '../services/analytics'
 import { FirebaseProvider } from '../services/firebase'
 import { LFCABackendProvider } from '../services/lfca-backend'
-import { ACTIONS } from '../utils/routes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  const { resetPosition } = useScrollPosition(LS_ACTION_LIST, false)
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      // track page view
-      trackEvent({ name: 'pageView' })
-      // only persist the browsing state when the user goes from
-      // the dashboard to the action detail page and back
-      // in all other cases, reset the browsing state
-      const fromDashboardToDetails =
-        router.pathname === ACTIONS && url.startsWith('/action/')
-      const fromDetailPageToDashboard =
-        router.pathname.startsWith('/action/') && url === ACTIONS
-
-      if (!(fromDashboardToDetails || fromDetailPageToDashboard)) {
-        resetPosition(INITIAL_VALUES)
-      }
-    }
-
-    router.events.on('routeChangeStart', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange)
-    }
-  }, [router.pathname, resetPosition, router.events])
-
   return (
     <AppProvider>
       <FirebaseProvider>
