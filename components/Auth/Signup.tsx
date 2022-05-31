aa9ce513784264be2ac57ca6c09db3c2557dea20
Timo Müller <timo.h.mueller@gmail.com>
2022-05-31 13:09:18 +0300

    adjust linter

--- components/Auth/Signup.tsx
+++ components/Auth/Signup.tsx
@@ -1,22 +1,20 @@
-import { Alert, Button, Form, Input, message } from 'antd'
+import { Button, Form, Input, message } from 'antd'
 import NextLink from 'next/link'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 import { useFirebase } from '../../hooks/firebase'
 import { useRegisterUserMutation } from '../../services/lfca-backend'
 import { getErrorMessage } from '../../utils'
 import {
-  SIGN_IN,
   ONBOARDING_LEADER,
   ONBOARDING_OFFICER,
+  SIGN_IN,
 } from '../../utils/routes'
 import { CLOUDINARY_PRESETS } from '../FileUpload/helper'
 import { ImageUpload } from '../FileUpload/ImageUpload'
-import { useRouter } from 'next/router'
 
 export const Signup = ({ email }: { email: string }) => {
-  const [success, setSuccess] = useState(false)
   const [{ fetching }, registerUser] = useRegisterUserMutation()
   const { login } = useFirebase()
   const router = useRouter()
@@ -47,7 +45,6 @@ export const Signup = ({ email }: { email: string }) => {
         },
       })
       await login(email, password)
-      console.log('data', data)
       // forward based on user role
       if (data?.registerUser.roles.includes('LEADER')) {
         router.push(ONBOARDING_LEADER)
@@ -65,83 +62,64 @@ export const Signup = ({ email }: { email: string }) => {
     })
   }, [email, form])
 
-  if (!success) {
-    return (
-      <div>
-        <h1>Create account</h1>
-        <Form
-          form={form}
-          initialValues={{ email }}
-          layout="vertical"
-          onFinish={handleSignUp}
+  return (
+    <div>
+      <h1>Create account</h1>
+      <Form
+        form={form}
+        initialValues={{ email }}
+        layout="vertical"
+        onFinish={handleSignUp}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ message: 'Please input your email!', required: true }]}
         >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ message: 'Please input your email!', required: true }]}
-          >
-            <Input disabled placeholder="greta@thunberg.earth" type="email" />
-          </Form.Item>
+          <Input disabled placeholder="greta@thunberg.earth" type="email" />
+        </Form.Item>
 
-          <Form.Item
-            label="First name"
-            name="firstName"
-            rules={[{ message: 'Please input your name!', required: true }]}
-          >
-            <Input placeholder="Greta" />
-          </Form.Item>
+        <Form.Item
+          label="First name"
+          name="firstName"
+          rules={[{ message: 'Please input your name!', required: true }]}
+        >
+          <Input placeholder="Greta" />
+        </Form.Item>
 
-          <Form.Item
-            label="Last name"
-            name="lastName"
-            rules={[{ message: 'Please input your name!', required: true }]}
-          >
-            <Input placeholder="Thunberg" />
-          </Form.Item>
+        <Form.Item
+          label="Last name"
+          name="lastName"
+          rules={[{ message: 'Please input your name!', required: true }]}
+        >
+          <Input placeholder="Thunberg" />
+        </Form.Item>
 
-          <Form.Item
-            key="picture"
-            label="Picture"
-            name="picture"
-            rules={[{ message: 'Please add a picture', required: true }]}
-          >
-            <ImageUpload customPreset={CLOUDINARY_PRESETS.profilePictures} />
-          </Form.Item>
+        <Form.Item
+          key="picture"
+          label="Picture"
+          name="picture"
+          rules={[{ message: 'Please add a picture', required: true }]}
+        >
+          <ImageUpload customPreset={CLOUDINARY_PRESETS.profilePictures} />
+        </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ message: 'Please input a password!', required: true }]}
-          >
-            <Input.Password placeholder="********" />
-          </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ message: 'Please input a password!', required: true }]}
+        >
+          <Input.Password placeholder="********" />
+        </Form.Item>
 
-          <Form.Item>
-            <Button block htmlType="submit" loading={fetching} type="primary">
-              Register
-            </Button>
-          </Form.Item>
-        </Form>
-
-        <NextLink href={SIGN_IN}>Already registered? Sign in here</NextLink>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>Create account</h1>
-        <Alert
-          description="Your account has been successfully created. You can now sign in."
-          message="Account created"
-          showIcon
-          type="success"
-        />
-        <Link href={SIGN_IN}>
-          <Button block style={{ margin: '30px 0' }} type="primary">
-            Sign in
+        <Form.Item>
+          <Button block htmlType="submit" loading={fetching} type="primary">
+            Register
           </Button>
-        </Link>
-      </div>
-    )
-  }
+        </Form.Item>
+      </Form>
+
+      <NextLink href={SIGN_IN}>Already registered? Sign in here</NextLink>
+    </div>
+  )
 }
