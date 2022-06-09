c877d7bccd9cfb354e444da7784907f7cf6e70d5
David Ruisinger <david.ruisinger@gmail.com>
2022-06-09 17:50:16 +0200

    feat: maintenance mode (#40)

---
+++ pages/maintenance.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import React from 'react'
+
+import { OneColLayout } from '../components/Layout'
+
+const Maintenance: NextPage = () => {
+  return (
+    <OneColLayout>
+      <h1>We&apos;ll be back soon...</h1>
+    </OneColLayout>
+  )
+}
+
+export default Maintenance
