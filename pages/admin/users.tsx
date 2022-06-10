385627b71dfc585d899b86931049365a2a9aa25a
David Ruisinger <david.ruisinger@gmail.com>
2022-06-10 14:37:52 +0200

    feat: search & paginate users

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,7 +1,8 @@
 import { PlusOutlined } from '@ant-design/icons'
 import { Button, Drawer, Input, List, message, Space } from 'antd'
+import _debounce from 'lodash.debounce'
 import type { GetStaticProps, NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { InviteUserForm } from '../../components/InviteUserForm'
 import { Main, Section, SiderLayout } from '../../components/Layout'
@@ -9,8 +10,9 @@ import { UserForm } from '../../components/UserForm'
 import { Country, fetchAllCountries } from '../../services/contentful'
 import {
   UpdateUserInput,
-  useAllUsersQuery,
+  useSearchUserQuery,
   useUpdateUserMutation,
+  useUsersQuery,
 } from '../../services/lfca-backend'
 import { ADMIN_NAV } from '../../utils/navs'
 import { withAuth } from '../../utils/with-auth'
@@ -25,11 +27,57 @@ interface AdminUsersProps {
 const AdminUsers: NextPage<AdminUsersProps> = ({ countries }) => {
   const [selectedUser, setSelectedUser] = useState<UserFragment | undefined>()
   const [isOpen, setIsOpen] = useState(false)
+  const [uidInput, setUidInput] = useState<string>('')
+  const [uidFilter, setUidFilter] = useState<string>('')
+  const [nameInput, setNameInput] = useState<string>('')
+  const [nameFilter, setNameFilter] = useState<string>('')
+  const [cursor, setCursor] = useState<string | null>(null)
+  const [cursorHistory, setCursorHistory] = useState<(string | null)[]>([])
+
+  const debouncedUidFilter = useRef(
+    _debounce(async (value) => {
+      setUidFilter(value)
+    }, 500)
+  ).current
+
+  const debouncedNameFilter = useRef(
+    _debounce(async (value) => {
+      setNameFilter(value)
+    }, 500)
+  ).current
+
+  useEffect(() => {
+    debouncedUidFilter(uidInput)
+  }, [debouncedUidFilter, uidInput])
+
+  useEffect(() => {
+    debouncedNameFilter(nameInput)
+  }, [debouncedNameFilter, nameInput])
 
   const [{ fetching: isUpdatingUser }, updateUser] = useUpdateUserMutation()
-  const [{ data: usersData, fetching: isFetchingUsers }] = useAllUsersQuery()
 
-  const users = usersData?.users.items || []
+  const [{ data: usersData, fetching: isFetchingUsers }] = useUsersQuery({
+    pause: !!nameFilter,
+    variables: {
+      input: {
+        cursor,
+        filter: {
+          userIds: uidFilter ? [uidFilter] : undefined,
+        },
+      },
+    },
+  })
+
+  const [{ data: searchData, fetching: isFetchingSearch }] = useSearchUserQuery(
+    {
+      pause: !nameFilter,
+      variables: {
+        input: {
+          query: nameFilter,
+        },
+      },
+    }
+  )
 
   const handleOpen = (user?: UserFragment) => {
     setIsOpen(true)
@@ -60,12 +108,33 @@ const AdminUsers: NextPage<AdminUsersProps> = ({ countries }) => {
             >
               Create new user
             </Button>
-            <Search placeholder="Search by uid" />
+            <Search
+              allowClear
+              onChange={(e) => {
+                setUidInput(e.target.value)
+                setNameInput('')
+              }}
+              placeholder="Search by uid"
+              value={uidInput}
+            />
+            or
+            <Search
+              allowClear
+              onChange={(e) => {
+                setNameInput(e.target.value)
+                setUidInput('')
+              }}
+              placeholder="Search by name"
+              value={nameInput}
+            />
           </Space>
-
           <List
-            dataSource={users}
-            loading={isFetchingUsers}
+            dataSource={
+              nameFilter
+                ? searchData?.searchUser || []
+                : usersData?.users.items || []
+            }
+            loading={isFetchingUsers || isFetchingSearch}
             renderItem={(user) => (
               <List.Item
                 actions={[
@@ -74,10 +143,31 @@ const AdminUsers: NextPage<AdminUsersProps> = ({ countries }) => {
                   </Button>,
                 ]}
               >
-                {user.firstName}
+                {user.firstName} {user.lastName}
               </List.Item>
             )}
           />
+          {!nameFilter && cursorHistory.length ? (
+            <Button
+              onClick={() => {
+                const [prevCursor, ...remainingHistory] = cursorHistory
+                setCursorHistory(remainingHistory)
+                setCursor(prevCursor)
+              }}
+            >
+              Previous Page
+            </Button>
+          ) : null}
+          {!nameFilter && usersData?.users.cursor ? (
+            <Button
+              onClick={() => {
+                setCursorHistory((v) => [cursor, ...v])
+                setCursor(usersData.users.cursor ?? null)
+              }}
+            >
+              Next Page
+            </Button>
+          ) : null}
           <Drawer
             className="drawer-md"
             destroyOnClose
