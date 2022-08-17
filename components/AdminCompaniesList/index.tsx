bec4c3856f0e1dd67c3aeb71f85d8bcb4b09685f
David Ruisinger <david.ruisinger@gmail.com>
2022-08-17 13:27:02 +0200

    fix: filtrrs in CompanyForm

--- components/AdminCompaniesList/index.tsx
+++ components/AdminCompaniesList/index.tsx
@@ -279,7 +279,7 @@ export const AdminCompaniesList = ({
                     'logoUrl',
                     'name',
                     'programContentId',
-                    'tags',
+                    'companyTags',
                     'websiteUrl',
                     'subscriptionType',
                   ]
