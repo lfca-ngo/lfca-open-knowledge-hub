a26705794cb98941572e452e5ec0479cacadb5ab
David Ruisinger <david.ruisinger@gmail.com>
2022-08-23 17:13:18 +0200

    feat: allow admins to manually add event attendees

--- components/AdminEventParticipants/index.tsx
+++ components/AdminEventParticipants/index.tsx
@@ -1,14 +1,18 @@
 require('./styles.less')
 
-import { UserOutlined } from '@ant-design/icons'
-import { Avatar, Button, List, message } from 'antd'
+import { CheckOutlined, DeleteOutlined, UserOutlined } from '@ant-design/icons'
+import { Avatar, Button, Form, List, message, Popconfirm } from 'antd'
 import { useState } from 'react'
 
 import {
+  EventParticipationStatus,
+  useCreateEventParticipationRequestMutation,
+  useDeleteEventParticipationRequestMutation,
   useEventParticipationRequestsQuery,
   useUpdateEventParticipationRequestMutation,
 } from '../../services/lfca-backend'
 import { EventFragment } from '../../services/lfca-backend'
+import { UserIdSearchInput } from '../UserIdSearchInput'
 
 interface AdminEventParticipantsProps {
   event?: EventFragment
@@ -21,6 +25,11 @@ export const AdminEventParticipants = ({
   const [updatingRequestId, setUpdatingRequestId] = useState<string | null>(
     null
   )
+  const [deletingRequestId, setDeletingRequestId] = useState<string | null>(
+    null
+  )
+
+  const [form] = Form.useForm()
 
   const [{ data, fetching: fetchingRequests }] =
     useEventParticipationRequestsQuery({
@@ -32,8 +41,31 @@ export const AdminEventParticipants = ({
       },
     })
 
+  const [
+    { fetching: fetchingCreateEventParticipationRequest },
+    createEventParticipationRequest,
+  ] = useCreateEventParticipationRequestMutation()
   const [, updateEventParticipationRequest] =
     useUpdateEventParticipationRequestMutation()
+  const [, deleteEventParticipationRequest] =
+    useDeleteEventParticipationRequestMutation()
+
+  const handleCreate = async ({ userId }: { userId?: string }) => {
+    if (!event) return
+    const res = await createEventParticipationRequest({
+      input: {
+        approved: true,
+        eventId: event.id,
+        userId,
+      },
+    })
+
+    if (res.error) {
+      message.error(res.error.message)
+    } else {
+      form.resetFields()
+    }
+  }
 
   const handleUpdate = async (
     eventParticipationRequestId: string,
@@ -54,9 +86,42 @@ export const AdminEventParticipants = ({
     setUpdatingRequestId(null)
   }
 
+  const handleDelete = async (eventParticipationRequestId: string) => {
+    setDeletingRequestId(eventParticipationRequestId)
+    const res = await deleteEventParticipationRequest({
+      input: {
+        eventParticipationRequestId,
+      },
+    })
+
+    if (res.error) {
+      message.error(res.error.message)
+    }
+
+    setDeletingRequestId(null)
+  }
+
   return (
     <div className="admin-event-participants">
       <h1>{event?.title}</h1>
+      <Form form={form} layout="vertical" onFinish={handleCreate}>
+        <Form.Item
+          name="userId"
+          rules={[{ message: 'Please select a user', required: true }]}
+        >
+          <UserIdSearchInput />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            htmlType="submit"
+            loading={fetchingCreateEventParticipationRequest}
+            type="primary"
+          >
+            Invite
+          </Button>
+        </Form.Item>
+      </Form>
       <List
         dataSource={data?.eventParticipationRequests || []}
         itemLayout="horizontal"
@@ -64,29 +129,44 @@ export const AdminEventParticipants = ({
         renderItem={(request) => (
           <List.Item
             actions={
-              request.status === 'PENDING'
+              request.status === EventParticipationStatus.PENDING
                 ? [
+                    <Popconfirm
+                      cancelText="No"
+                      key="delete"
+                      okText="Yes"
+                      onConfirm={() => handleDelete(request.id)}
+                      title="Are you sure to delete this request?"
+                    >
+                      <Button
+                        icon={<DeleteOutlined />}
+                        loading={deletingRequestId === request.id}
+                        type="ghost"
+                      />
+                    </Popconfirm>,
                     <Button
+                      icon={<CheckOutlined />}
                       key="approve"
                       loading={updatingRequestId === request.id}
                       onClick={() => handleUpdate(request.id, true)}
                       type="primary"
-                    >
-                      Approve
-                    </Button>,
+                    />,
                   ]
-                : request.status === 'APPROVED'
-                ? [
-                    <Button
-                      key="un-approve"
-                      loading={updatingRequestId === request.id}
-                      onClick={() => handleUpdate(request.id, false)}
-                      type="primary"
+                : [
+                    <Popconfirm
+                      cancelText="No"
+                      key="delete"
+                      okText="Yes"
+                      onConfirm={() => handleDelete(request.id)}
+                      title="Are you sure to delete this request?"
                     >
-                      Approve
-                    </Button>,
+                      <Button
+                        icon={<DeleteOutlined />}
+                        loading={deletingRequestId === request.id}
+                        type="ghost"
+                      />
+                    </Popconfirm>,
                   ]
-                : undefined
             }
           >
             <List.Item.Meta
