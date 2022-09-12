649f7f2cc060c9b412d4de09de16b22144b27487
Timo Müller <timo.h.mueller@gmail.com>
2022-09-12 12:03:47 +0300

    Feat/active group on dashboard (#77)
    * move event helper
    
    * show groups on dashboard

--- components/EventCard/EventCardDefault.tsx
+++ components/EventCard/EventCardDefault.tsx
@@ -40,65 +40,61 @@ export const EventCardDefault = ({
   }
 
   return (
-    <>
-      <Card
-        className="event-card default"
-        hoverable
-        onClick={onClick}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <div className="header">
-          <div className="icon">
-            <Avatar
-              className="wine-inverse"
-              icon={
-                isHovered ? (
-                  <span className="hover-view">
-                    <EyeOutlined />
-                    <span className="text">view details</span>
-                  </span>
-                ) : (
-                  matchStringToIcon(event.title)
-                )
-              }
-              shape="square"
-              size={90}
-            />
-          </div>
-          <div className="summary">
-            <div className="title">{event.title}</div>
-            <div className="info">
-              <div className="event-meta">
-                <Space size="large">
-                  <Status event={event} />
-                  <Recurrence event={event} />
-                </Space>
-              </div>
-              <div className="participants">
-                <LogoGroup
-                  data={event?.participationRequests.map(
-                    (r) => r?.user?.company
-                  )}
-                  size={35}
-                />
-              </div>
+    <Card
+      className="event-card default"
+      hoverable
+      onClick={onClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="header">
+        <div className="icon">
+          <Avatar
+            className="wine-inverse"
+            icon={
+              isHovered ? (
+                <span className="hover-view">
+                  <EyeOutlined />
+                  <span className="text">view details</span>
+                </span>
+              ) : (
+                matchStringToIcon(event.title)
+              )
+            }
+            shape="square"
+            size={90}
+          />
+        </div>
+        <div className="summary">
+          <div className="title">{event.title}</div>
+          <div className="info">
+            <div className="event-meta">
+              <Space size="large">
+                <Status event={event} />
+                <Recurrence event={event} />
+              </Space>
+            </div>
+            <div className="participants">
+              <LogoGroup
+                data={event?.participationRequests.map((r) => r?.user?.company)}
+                size={35}
+              />
             </div>
           </div>
-          <div className="actions">
-            <Space>
-              <ToggleSubscribeButton
-                buttonProps={{
-                  disabled:
-                    (hasAppliedForAtLeastOneEvent && !isPending) ||
-                    isParticipatingAtLeastOneEvent,
-                }}
-                event={event}
-              />
-            </Space>
-          </div>
         </div>
-      </Card>
-    </>
+        <div className="actions">
+          <Space>
+            <ToggleSubscribeButton
+              buttonProps={{
+                disabled:
+                  (hasAppliedForAtLeastOneEvent && !isPending) ||
+                  isParticipatingAtLeastOneEvent,
+              }}
+              event={event}
+            />
+          </Space>
+        </div>
+      </div>
+    </Card>
   )
 }
