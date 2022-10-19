a263784e90104542cce4bec8f97c2383fff6eba8
David Ruisinger <david.ruisinger@gmail.com>
2022-10-19 10:56:36 +0200

    feat: rsvp from within app

--- components/EventCard/ToggleSubscribeButton.tsx
+++ components/EventCard/ToggleSubscribeButton.tsx
@@ -1,5 +1,10 @@
-import { CloseCircleOutlined, UserAddOutlined } from '@ant-design/icons'
-import { Button, ButtonProps, message, Popover } from 'antd'
+import {
+  CheckOutlined,
+  CloseCircleOutlined,
+  StopOutlined,
+  UserAddOutlined,
+} from '@ant-design/icons'
+import { Button, ButtonProps, message, Popover, Space } from 'antd'
 import { SyntheticEvent } from 'react'
 
 import {
@@ -7,6 +12,7 @@ import {
   EventParticipantStatus,
   useAddEventParticipantMutation,
   useRemoveEventParticipantMutation,
+  useUpdateEventParticipantStatusMutation,
 } from '../../services/lfca-backend'
 
 export interface ToggleSubscribeButtonProps {
@@ -18,16 +24,20 @@ export const ToggleSubscribeButton = ({
   buttonProps,
   event,
 }: ToggleSubscribeButtonProps) => {
-  const isNotRequested =
+  const hasNotJoined =
     event.participationStatus === null ||
     event.participationStatus === EventParticipantStatus.USER_RSVP_DECLINED
-  const isPending =
+  const isPendingAdminApproval =
     event.participationStatus === EventParticipantStatus.AWAITING_ADMIN_APPROVAL
+  const isPendingUserRSVP =
+    event.participationStatus === EventParticipantStatus.AWAITING_USER_RSVP
 
   const [{ fetching: deleting }, removeEventParticipant] =
     useRemoveEventParticipantMutation()
   const [{ fetching: subscribing }, addEventParticipant] =
     useAddEventParticipantMutation()
+  const [{ fetching: updating }, updateEventParticipantStatus] =
+    useUpdateEventParticipantStatusMutation()
 
   const handleJoin = async () => {
     if (
@@ -69,10 +79,27 @@ export const ToggleSubscribeButton = ({
     }
   }
 
+  const handleRSVP = async (accept: boolean) => {
+    const res = await updateEventParticipantStatus({
+      input: {
+        eventId: event.id,
+        status: accept
+          ? EventParticipantStatus.USER_RSVP_ACCEPTED
+          : EventParticipantStatus.USER_RSVP_DECLINED,
+      },
+    })
+
+    if (res.error) {
+      message.error(res.error.message)
+    } else {
+      message.success('Your RSVP has been saved')
+    }
+  }
+
   const changeEventSubscription = (e: SyntheticEvent) => {
     e.stopPropagation()
 
-    if (isNotRequested) {
+    if (hasNotJoined) {
       handleJoin()
     } else {
       handleDelete()
@@ -80,7 +107,9 @@ export const ToggleSubscribeButton = ({
   }
 
   const popoverContent = () => {
-    if (isPending) return 'Your application is pending'
+    if (isPendingAdminApproval) return 'Your application is pending'
+    if (isPendingUserRSVP)
+      return 'You are invited to join. Let us know if you are coming!'
     if (buttonProps?.disabled)
       return 'You can only join one group. Unsubscribe from your group first.'
     else return null
@@ -88,16 +117,35 @@ export const ToggleSubscribeButton = ({
 
   return (
     <Popover content={popoverContent()}>
-      <Button
-        icon={isNotRequested ? <UserAddOutlined /> : <CloseCircleOutlined />}
-        key="unsubscribe"
-        loading={subscribing || deleting}
-        onClick={changeEventSubscription}
-        type={isNotRequested ? 'primary' : 'default'}
-        {...buttonProps}
-      >
-        {isNotRequested ? 'Join' : 'Unsubscribe'}
-      </Button>
+      {isPendingUserRSVP ? (
+        <Space>
+          <Button
+            icon={<StopOutlined />}
+            key="decline"
+            loading={updating}
+            onClick={() => handleRSVP(false)}
+            type="default"
+          />
+          <Button
+            icon={<CheckOutlined />}
+            key="accept"
+            loading={updating}
+            onClick={() => handleRSVP(true)}
+            type="primary"
+          />
+        </Space>
+      ) : (
+        <Button
+          icon={hasNotJoined ? <UserAddOutlined /> : <CloseCircleOutlined />}
+          key="unsubscribe"
+          loading={subscribing || deleting}
+          onClick={changeEventSubscription}
+          type={hasNotJoined ? 'primary' : 'default'}
+          {...buttonProps}
+        >
+          {hasNotJoined ? 'Join' : 'Unsubscribe'}
+        </Button>
+      )}
     </Popover>
   )
 }
