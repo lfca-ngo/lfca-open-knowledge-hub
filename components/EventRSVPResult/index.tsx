0a0899a05a187c2ffedf13149fb251b523551756
Timo Müller <timo.h.mueller@gmail.com>
2022-10-24 16:14:46 +0300

    adjust response form

--- components/EventRSVPResult/index.tsx
+++ components/EventRSVPResult/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Skeleton } from 'antd'
+import { Alert, Button, Form, Input, Select, Skeleton } from 'antd'
 import Link from 'next/link'
 import React from 'react'
 
@@ -18,17 +18,49 @@ interface EventRSVPResultProps {
   token?: string | string[]
 }
 
+const DECLINE_OPTIONS = [
+  {
+    key: 'time',
+    label:
+      'Time: The format and topic are interesting, but the suggested time does not fit',
+  },
+  {
+    key: 'sector',
+    label:
+      'Sector: The format is interesting, but I need a group focused on another sector',
+  },
+  {
+    key: 'format',
+    label: 'Format: This group format does not fit my needs',
+  },
+  {
+    key: 'priority',
+    label:
+      'Priority: Climate action is not a priority for me/our company right now',
+  },
+  {
+    key: 'other',
+    label: 'Other',
+  },
+]
+
 export const EventRSVPResult = ({
   event,
   hasError,
   isFetching,
   token,
 }: EventRSVPResultProps) => {
+  const [form] = Form.useForm<{ reason: string; notes: string }>()
+  const reasonValue = Form.useWatch('reason', form)
+
   const [{ data, fetching: isSubmittingNotes }, updateTokenRSVPWithNotes] =
     useProcessEventRsvpTokenMutation()
 
-  const handleSubmit = async (allValues: { notes?: string }) => {
-    const notes = allValues.notes
+  const handleSubmit = async (allValues: {
+    notes?: string
+    reason?: string
+  }) => {
+    const notes = `${allValues.reason} ${allValues.notes || ''}`
 
     if (!notes || typeof token !== 'string') return
 
@@ -67,16 +99,51 @@ export const EventRSVPResult = ({
       ) : event?.participationStatus ===
         EventParticipantStatus.USER_RSVP_DECLINED ? (
         <>
-          <h1>Thank you!</h1>
+          <h1>Thanks for your response!</h1>
+          <p style={{ marginBottom: '30px' }}>
+            Please let us know why you declined the invitation to join this
+            Mastermind Group. This will help us to improve our service and offer
+            better solutions for you in the future.
+          </p>
           {!data?.processEventRSVPToken ? (
-            <Form layout="vertical" onFinish={handleSubmit}>
+            <Form form={form} layout="vertical" onFinish={handleSubmit}>
               <Form.Item
-                key="notes"
-                label="Let us know why you can not join"
-                name="notes"
+                key="reason"
+                label="Reason for declining"
+                name="reason"
+                rules={[
+                  {
+                    message: 'Please select an option',
+                    required: true,
+                  },
+                ]}
               >
-                <Input.TextArea placeholder="e.g. time does not fit my calendar" />
+                <Select
+                  placeholder="Select an option"
+                  popupClassName="multi-row-options"
+                >
+                  {DECLINE_OPTIONS.map((option) => (
+                    <Select.Option key={option.key}>
+                      {option.label}
+                    </Select.Option>
+                  ))}
+                </Select>
               </Form.Item>
+              {reasonValue === 'other' && (
+                <Form.Item
+                  key="notes"
+                  label="Other"
+                  name="notes"
+                  rules={[
+                    {
+                      message: 'Please share why you can not join',
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Input.TextArea placeholder="e.g. time does not fit my calendar" />
+                </Form.Item>
+              )}
 
               <Form.Item>
                 <Button
@@ -89,7 +156,12 @@ export const EventRSVPResult = ({
               </Form.Item>
             </Form>
           ) : (
-            <p>We received your feedback.</p>
+            <Alert
+              message="We received your feedback."
+              showIcon
+              style={{ margin: '0 0 20px' }}
+              type="success"
+            />
           )}
 
           <p>
