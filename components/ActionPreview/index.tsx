345b90131af56a36d42b86675b43518c478ac3b2
Timo Müller <timo.h.mueller@gmail.com>
2022-10-27 14:26:29 +0300

    simple preview module for onboarding

--- components/ActionPreview/index.tsx
+++ components/ActionPreview/index.tsx
@@ -1,10 +1,14 @@
+import { InfoCircleOutlined } from '@ant-design/icons'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { Divider, Popover, Tag } from 'antd'
+import { useState } from 'react'
 
 import { ContentfulActionFields } from '../../services/contentful'
 import { CompanyActionListItemFragment } from '../../services/lfca-backend'
 import { DEFAULT_FONT_SIZE, DEFAULT_LINE_HEIGHT } from '../../utils'
 import { options } from '../../utils/richTextOptions'
-import { RequirementsList } from '../RequirementsListTabs'
+import { Section } from '../Layout'
+import { RequirementsListTabs } from '../RequirementsListTabs'
 import { ShowMore } from '../ShowMore'
 import styles from './styles.module.less'
 
@@ -17,25 +21,39 @@ export const ActionPreview = ({
   action,
   actionContent,
 }: ActionPreviewProps) => {
+  const [activeStatusTab, setActiveStatusTab] = useState<string | undefined>()
+
   return (
     <div className={styles['action-preview']}>
-      <h2>{actionContent.title}</h2>
+      <Popover content="You can view the entire module on your dashboard">
+        <Tag className="super-text" icon={<InfoCircleOutlined />}>
+          Module Summary
+        </Tag>
+      </Popover>
 
-      <ShowMore
-        buttonProps={{ type: 'link' }}
-        maskMode="transparent"
-        maxHeight={DEFAULT_FONT_SIZE * DEFAULT_LINE_HEIGHT * 4}
-        text={
-          actionContent?.aboutText &&
-          documentToReactComponents(actionContent?.aboutText, options)
-        }
-      />
+      <h1>{actionContent.title}</h1>
 
-      <RequirementsList
-        actionContentId={actionContent.actionId}
-        requirements={action.requirements}
-        requirementsContent={actionContent.requirements}
-      />
+      <Section>
+        <ShowMore
+          buttonProps={{ type: 'link' }}
+          maskMode="transparent"
+          maxHeight={DEFAULT_FONT_SIZE * DEFAULT_LINE_HEIGHT * 4}
+          text={
+            actionContent?.aboutText &&
+            documentToReactComponents(actionContent?.aboutText, options)
+          }
+        />
+      </Section>
+
+      <Divider />
+      <Section title="How To" titleSize="small">
+        <RequirementsListTabs
+          action={action}
+          actionContent={actionContent}
+          activeStatusTab={activeStatusTab}
+          setActiveStatusTab={setActiveStatusTab}
+        />
+      </Section>
     </div>
   )
 }
