36d36c85bcbe7323e65d09d4168cd7283f8b7c96
David Ruisinger <david.ruisinger@gmail.com>
2022-11-14 15:18:31 +0100

    feat: support legacy rsvp tokens

--- pages/event-rsvp.tsx
+++ pages/event-rsvp.tsx
@@ -1,22 +1,80 @@
+import { Skeleton } from 'antd'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import React from 'react'
 
-import { EventRSVPResult } from '../components/EventRSVPResult'
 import { OneColLayout } from '../components/Layout'
-import { withEventToken } from '../hooks/useProcessEventToken'
 import EventBackgroundImage from '../public/img/event-bg-image.png'
-
-const WrappedEventRsvpResult = withEventToken(EventRSVPResult)
+import { EventParticipantStatus } from '../services/lfca-backend'
+import { DEFAULT_SUPPORT_EMAIL } from '../utils'
 
 /**
- * IMPORTANT: Page will be deprecated in
- * favor of the new events/ pages
+ * NOTE:
+ * This page is deprecated and request will be forwareded to
+ * `events/rsvp`
  */
-
 const EventRsvp: NextPage = () => {
+  const { isReady, query, replace } = useRouter()
+  const { token } = query
+  const [hasError, setHasError] = React.useState(false)
+
+  React.useEffect(() => {
+    if (!isReady) return
+    if (typeof token !== 'string') return setHasError(true)
+
+    // With legacy tokens, status was updated based on token payload status.
+    // So we need to extract the status from the payload to be able to redirect
+    const decodedToken = jwt.decode(token) as JwtPayload & {
+      status?: EventParticipantStatus
+    }
+    if (
+      !decodedToken ||
+      !decodedToken.status ||
+      (decodedToken.status !== EventParticipantStatus.USER_RSVP_ACCEPTED &&
+        decodedToken.status !== EventParticipantStatus.USER_RSVP_DECLINED)
+    ) {
+      return setHasError(true)
+    }
+
+    replace({
+      pathname: '/events/rsvp',
+      query: {
+        method:
+          decodedToken.status === EventParticipantStatus.USER_RSVP_ACCEPTED
+            ? 'accept'
+            : 'decline',
+        token,
+      },
+    })
+  }, [replace, token, isReady])
+
   return (
     <OneColLayout backgroundImage={EventBackgroundImage}>
-      <WrappedEventRsvpResult />
+      {hasError ? (
+        <>
+          <h1>Error!!!</h1>
+          <p>
+            Please try again. If the problem persists, please contact us via{' '}
+            <a href={`mailto:${DEFAULT_SUPPORT_EMAIL}`}>
+              {DEFAULT_SUPPORT_EMAIL}
+            </a>
+            .
+          </p>
+        </>
+      ) : (
+        <Skeleton
+          active
+          loading={true}
+          paragraph={{
+            rows: 2,
+            width: '100%',
+          }}
+          title={{
+            width: '100%',
+          }}
+        />
+      )}
     </OneColLayout>
   )
 }
--- pages/events/rsvp.tsx
+++ pages/events/rsvp.tsx
@@ -3,18 +3,19 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-import { EventRSVPResult } from '../../../components/EventRSVPResult'
-import { OneColLayout } from '../../../components/Layout'
-import EventBackgroundImage from '../../../public/img/event-bg-image.png'
+import { EventRSVPResult } from '../../components/EventRSVPResult'
+import { OneColLayout } from '../../components/Layout'
+import EventBackgroundImage from '../../public/img/event-bg-image.png'
 import {
   EventParticipantStatus,
   useProcessEventRsvpTokenMutation,
-} from '../../../services/lfca-backend'
-import { DEFAULT_SUPPORT_EMAIL } from '../../../utils'
+} from '../../services/lfca-backend'
+import { DEFAULT_SUPPORT_EMAIL } from '../../utils'
 
 const EventRSVPPage: NextPage = () => {
   const { isReady, query } = useRouter()
   const { method, token } = query
+  console.log(query)
 
   const [{ data, error, fetching }, processEventRSVPToken] =
     useProcessEventRsvpTokenMutation()
@@ -38,38 +39,34 @@ const EventRSVPPage: NextPage = () => {
     })
   }, [method, processEventRSVPToken, token])
 
-  if (fetching || !isReady)
-    return (
-      <Skeleton
-        active
-        loading={fetching}
-        paragraph={{
-          rows: 2,
-          width: '100%',
-        }}
-        title={{
-          width: '100%',
-        }}
-      />
-    )
-
-  if (error)
-    return (
-      <>
-        <h1>Something went wrong...</h1>
-        <p>
-          Please try again. If the problem persists, please contact us via{' '}
-          <a href={`mailto:${DEFAULT_SUPPORT_EMAIL}`}>
-            {DEFAULT_SUPPORT_EMAIL}
-          </a>
-          .
-        </p>
-      </>
-    )
-
   return (
     <OneColLayout backgroundImage={EventBackgroundImage}>
-      <EventRSVPResult event={data?.processEventRSVPToken} token={token} />
+      {fetching || !isReady ? (
+        <Skeleton
+          active
+          loading={fetching}
+          paragraph={{
+            rows: 2,
+            width: '100%',
+          }}
+          title={{
+            width: '100%',
+          }}
+        />
+      ) : error ? (
+        <>
+          <h1>Something went wrong...</h1>
+          <p>
+            Please try again. If the problem persists, please contact us via{' '}
+            <a href={`mailto:${DEFAULT_SUPPORT_EMAIL}`}>
+              {DEFAULT_SUPPORT_EMAIL}
+            </a>
+            .
+          </p>
+        </>
+      ) : (
+        <EventRSVPResult event={data?.processEventRSVPToken} token={token} />
+      )}
     </OneColLayout>
   )
 }
