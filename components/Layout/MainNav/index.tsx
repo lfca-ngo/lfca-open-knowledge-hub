304eb22cedf48bf304997f652651f58eeac65766
Timo Müller <timo.h.mueller@gmail.com>
2022-11-18 12:53:07 +0200

    add types to menu

--- components/Layout/MainNav/index.tsx
+++ components/Layout/MainNav/index.tsx
@@ -1,177 +1,35 @@
-import {
-  AppstoreAddOutlined,
-  AppstoreOutlined,
-  BankOutlined,
-  CalculatorOutlined,
-  CalendarOutlined,
-  CarryOutOutlined,
-  CheckCircleOutlined,
-  InsertRowLeftOutlined,
-  LockOutlined,
-  MessageOutlined,
-  ProfileOutlined,
-  QuestionCircleOutlined,
-  RocketOutlined,
-  SettingOutlined,
-  ShareAltOutlined,
-  StarOutlined,
-  TeamOutlined,
-  UsergroupAddOutlined,
-} from '@ant-design/icons'
-import { Menu, MenuProps } from 'antd'
+import { Menu } from 'antd'
+import { ItemType } from 'antd/lib/menu/hooks/useItems'
 import { useRouter } from 'next/router'
 import React, { useMemo } from 'react'
 
 import { useUser } from '../../../hooks/user'
 import {
-  ACHIEVEMENTS,
-  ACTIONS,
-  ACTIONS_COMPLETED,
-  ACTIONS_PLANNED,
-  ADMIN,
-  ADMIN_ACTIONS,
-  ADMIN_COMMENTS,
-  ADMIN_COMPANIES,
-  ADMIN_GROUPS,
-  ADMIN_REVIEWS,
-  ADMIN_SETTINGS,
-  ADMIN_USERS,
-  COMMUNITY_GROUPS,
-  PERSONAL_FOOTPRINT_CALCULATOR,
-  REFERRAL_PROGRAM,
-  SERVICE_PROVIDERS,
-  SUPPORT,
-  TOOLS,
-  USEFUL_LINKS,
-} from '../../../utils/routes'
+  NAV_ITEMS_ADMIN,
+  NAV_ITEMS_BEHIND_PAYWALL,
+  NAV_ITEMS_DEFAULT,
+} from '../../../utils/navs'
 import { PaywallPopover } from '../../PayWall/PaywallPopover'
 import styles from './styles.module.less'
-
-const NAV_ITEMS_BEHIND_PAYWALL = [ACHIEVEMENTS, TOOLS]
-
-const NAV_ITEMS_DEFAULT: MenuProps['items'] = [
-  {
-    children: [
-      {
-        icon: <ProfileOutlined />,
-        key: ACTIONS,
-        label: 'Overview',
-      },
-      {
-        icon: <CalendarOutlined />,
-        key: ACTIONS_PLANNED,
-        label: 'Planned actions',
-      },
-      {
-        icon: <CheckCircleOutlined />,
-        key: ACTIONS_COMPLETED,
-        label: 'Completed actions',
-      },
-    ],
-    icon: <CarryOutOutlined />,
-    key: 'dashboard',
-    label: 'Knowledge Hub',
-  },
-  {
-    icon: <TeamOutlined />,
-    key: COMMUNITY_GROUPS,
-    label: 'Mastermind Groups',
-  },
-  {
-    icon: <RocketOutlined />,
-    key: ACHIEVEMENTS,
-    label: 'Achievements',
-  },
-  {
-    children: [
-      {
-        icon: <AppstoreAddOutlined />,
-        key: SERVICE_PROVIDERS,
-        label: 'Tool Comparison',
-      },
-      {
-        icon: <CalculatorOutlined />,
-        key: PERSONAL_FOOTPRINT_CALCULATOR,
-        label: 'Personal Footprint Calculator',
-      },
-      {
-        icon: <ShareAltOutlined />,
-        key: REFERRAL_PROGRAM,
-        label: 'Referral',
-      },
-      {
-        icon: <ProfileOutlined />,
-        key: USEFUL_LINKS,
-        label: 'Links',
-      },
-      {
-        icon: <QuestionCircleOutlined />,
-        key: SUPPORT,
-        label: 'Help',
-      },
-    ],
-    icon: <InsertRowLeftOutlined />,
-    key: TOOLS,
-    label: 'Quick Access',
-  },
-]
-
-const NAV_ITEMS_ADMIN: MenuProps['items'] = [
-  {
-    children: [
-      {
-        icon: <UsergroupAddOutlined />,
-        key: ADMIN_USERS,
-        label: 'Users',
-      },
-      {
-        icon: <BankOutlined />,
-        key: ADMIN_COMPANIES,
-        label: 'Companies',
-      },
-      {
-        icon: <TeamOutlined />,
-        key: ADMIN_GROUPS,
-        label: 'Events',
-      },
-      {
-        icon: <StarOutlined />,
-        key: ADMIN_REVIEWS,
-        label: 'Reviews',
-      },
-      {
-        icon: <MessageOutlined />,
-        key: ADMIN_COMMENTS,
-        label: 'Comments',
-      },
-      {
-        icon: <AppstoreOutlined />,
-        key: ADMIN_ACTIONS,
-        label: 'Actions',
-      },
-      {
-        icon: <SettingOutlined />,
-        key: ADMIN_SETTINGS,
-        label: 'Settings',
-      },
-    ],
-    icon: <LockOutlined />,
-    key: ADMIN,
-    label: 'Admin',
-  },
-]
 
 export const MainNav = () => {
   const { isAdmin, isPaying } = useUser()
 
   const router = useRouter()
 
-  const addPaywall = (item: any) => {
-    if (NAV_ITEMS_BEHIND_PAYWALL.indexOf(item.key) > -1) {
+  const addPaywall = (item: ItemType) => {
+    if (NAV_ITEMS_BEHIND_PAYWALL.indexOf(`${item?.key}`) > -1) {
       return {
         ...item,
+        children: null,
         disabled: true,
-        icon: <PaywallPopover>{item.icon}</PaywallPopover>,
+        icon:
+          item && 'icon' in item ? (
+            <PaywallPopover>
+              <>{item?.icon}</>
+            </PaywallPopover>
+          ) : null,
+        key: `${item?.key}`,
       }
     } else return item
   }
@@ -185,7 +43,7 @@ export const MainNav = () => {
       menuItems = menuItems.map(addPaywall)
     }
     return menuItems
-  }, [isAdmin, isPaying]) as MenuProps['items']
+  }, [isAdmin, isPaying]) as ItemType[]
 
   const handleSelect = ({ key }: { key: string }) => {
     router.push(key)
--- utils/navs.tsx
+++ utils/navs.tsx
@@ -1,17 +1,36 @@
 import {
+  AppstoreAddOutlined,
+  AppstoreOutlined,
   BankOutlined,
+  CalculatorOutlined,
+  CalendarOutlined,
+  CarryOutOutlined,
+  CheckCircleOutlined,
+  InsertRowLeftOutlined,
   LikeOutlined,
   LoadingOutlined,
+  LockOutlined,
   LogoutOutlined,
+  MessageOutlined,
+  ProfileOutlined,
   QuestionCircleOutlined,
+  RocketOutlined,
+  SettingOutlined,
+  ShareAltOutlined,
+  StarOutlined,
+  TeamOutlined,
   ThunderboltOutlined,
+  UsergroupAddOutlined,
   UserOutlined,
 } from '@ant-design/icons'
+import { ItemType } from 'antd/lib/menu/hooks/useItems'
 
 import {
+  ACHIEVEMENTS,
   ACTIONS,
   ACTIONS_COMPLETED,
   ACTIONS_PLANNED,
+  ADMIN,
   ADMIN_ACTIONS,
   ADMIN_COMMENTS,
   ADMIN_COMPANIES,
@@ -20,35 +39,42 @@ import {
   ADMIN_SETTINGS,
   ADMIN_USERS,
   COMMUNITY_GROUPS,
+  PERSONAL_FOOTPRINT_CALCULATOR,
+  REFERRAL_PROGRAM,
+  SERVICE_PROVIDERS,
   SETTINGS,
   SETTINGS_COMPANY,
   SETTINGS_INVITE,
   SETTINGS_SUBSCRIPTION,
   SUPPORT,
+  TOOLS,
+  USEFUL_LINKS,
 } from './routes'
 
 export const LOGOUT = 'logout'
 
 export const ACTIONS_NAV = [
-  { key: ACTIONS, label: 'Dashboard' },
-  { key: ACTIONS_PLANNED, label: 'Planned Actions' },
-  { key: ACTIONS_COMPLETED, label: 'Completed Actions' },
+  { icon: <ProfileOutlined />, key: ACTIONS, label: 'Dashboard' },
+  {
+    icon: <CalendarOutlined />,
+    key: ACTIONS_PLANNED,
+    label: 'Planned Actions',
+  },
+  {
+    icon: <CheckCircleOutlined />,
+    key: ACTIONS_COMPLETED,
+    label: 'Completed Actions',
+  },
 ]
 
-// {
-//   icon: <AppstoreAddOutlined />,
-//   key: SERVICE_PROVIDERS,
-//   label: 'Tool Comparison',
-// }
-
 export const ADMIN_NAV = [
-  { key: ADMIN_USERS, label: 'Users' },
-  { key: ADMIN_COMPANIES, label: 'Companies' },
-  { key: ADMIN_GROUPS, label: 'Events' },
-  { key: ADMIN_REVIEWS, label: 'Reviews' },
-  { key: ADMIN_COMMENTS, label: 'Comments' },
-  { key: ADMIN_ACTIONS, label: 'Actions' },
-  { key: ADMIN_SETTINGS, label: 'Settings' },
+  { icon: <UsergroupAddOutlined />, key: ADMIN_USERS, label: 'Users' },
+  { icon: <BankOutlined />, key: ADMIN_COMPANIES, label: 'Companies' },
+  { icon: <TeamOutlined />, key: ADMIN_GROUPS, label: 'Events' },
+  { icon: <StarOutlined />, key: ADMIN_REVIEWS, label: 'Reviews' },
+  { icon: <MessageOutlined />, key: ADMIN_COMMENTS, label: 'Comments' },
+  { icon: <AppstoreOutlined />, key: ADMIN_ACTIONS, label: 'Actions' },
+  { icon: <SettingOutlined />, key: ADMIN_SETTINGS, label: 'Settings' },
 ]
 
 export const COMMUNITY_NAV = [{ key: COMMUNITY_GROUPS, label: 'Groups' }]
@@ -58,6 +84,34 @@ export const SETTINGS_NAV = [
   { key: SETTINGS_COMPANY, label: 'Edit Company' },
   { key: SETTINGS_INVITE, label: 'Invite Team' },
   { key: SETTINGS_SUBSCRIPTION, label: 'Your membership' },
+]
+
+export const OTHER_NAV = [
+  {
+    icon: <AppstoreAddOutlined />,
+    key: SERVICE_PROVIDERS,
+    label: 'Tool Comparison',
+  },
+  {
+    icon: <CalculatorOutlined />,
+    key: PERSONAL_FOOTPRINT_CALCULATOR,
+    label: 'Personal Footprint Calculator',
+  },
+  {
+    icon: <ShareAltOutlined />,
+    key: REFERRAL_PROGRAM,
+    label: 'Referral',
+  },
+  {
+    icon: <ProfileOutlined />,
+    key: USEFUL_LINKS,
+    label: 'Links',
+  },
+  {
+    icon: <QuestionCircleOutlined />,
+    key: SUPPORT,
+    label: 'Help',
+  },
 ]
 
 export const PROFILE_NAV = (loading: boolean) => [
@@ -90,5 +144,41 @@ export const PROFILE_NAV = (loading: boolean) => [
     icon: loading ? <LoadingOutlined /> : <LogoutOutlined />,
     key: LOGOUT,
     label: 'Logout',
+  },
+]
+
+export const NAV_ITEMS_BEHIND_PAYWALL = [ACHIEVEMENTS, TOOLS]
+
+export const NAV_ITEMS_DEFAULT: ItemType[] = [
+  {
+    children: ACTIONS_NAV,
+    icon: <CarryOutOutlined />,
+    key: 'dashboard',
+    label: 'Knowledge Hub',
+  },
+  {
+    icon: <TeamOutlined />,
+    key: COMMUNITY_GROUPS,
+    label: 'Mastermind Groups',
+  },
+  {
+    icon: <RocketOutlined />,
+    key: ACHIEVEMENTS,
+    label: 'Achievements',
+  },
+  {
+    children: OTHER_NAV,
+    icon: <InsertRowLeftOutlined />,
+    key: TOOLS,
+    label: 'Quick Access',
+  },
+]
+
+export const NAV_ITEMS_ADMIN: ItemType[] = [
+  {
+    children: ADMIN_NAV,
+    icon: <LockOutlined />,
+    key: ADMIN,
+    label: 'Admin',
   },
 ]
