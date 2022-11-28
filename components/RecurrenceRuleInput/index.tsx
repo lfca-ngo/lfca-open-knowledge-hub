177de53ec849d8d57766d0f190929503732da1d3
David Ruisinger <david.ruisinger@gmail.com>
2022-11-28 15:39:09 +0100

    feat: create/update onetime events

--- components/RecurrenceRuleInput/index.tsx
+++ components/RecurrenceRuleInput/index.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Input, InputNumber } from 'antd'
+import { Checkbox, Input, InputNumber, Switch } from 'antd'
 import { CheckboxChangeEvent } from 'antd/lib/checkbox'
 import React from 'react'
 
@@ -16,6 +16,14 @@ export const RecurrenceRuleInput = ({
   // If a value for COUNT is present, the event is NOT infinite
   const existingInterval = getNumberFromValueString(value, 'INTERVAL')
   const existingCount = getNumberFromValueString(value, 'COUNT')
+
+  const onSwitchChange = (checked: boolean) => {
+    if (checked) {
+      onChange?.(createValueString({ count: null, interval: 1 }))
+    } else {
+      onChange?.(null)
+    }
+  }
 
   const onIntervalChange = (newInterval: number | null) => {
     onChange?.(createValueString({ interval: newInterval }))
@@ -35,41 +43,46 @@ export const RecurrenceRuleInput = ({
 
   return (
     <>
-      <Input.Group className={styles.recurrenceInputGroup}>
-        <span>every </span>
-        <InputNumber
-          inputMode="numeric"
-          onChange={onIntervalChange}
-          pattern="[0-9]*"
-          placeholder="4"
-          type="number"
-          value={existingInterval}
-        />
-        <span>{` week${
-          existingInterval && existingInterval > 1 ? 's' : ''
-        }`}</span>
-
-        {existingCount ? (
-          <>
-            <span> for </span>
+      <Switch checked={!!value} onChange={onSwitchChange} />
+      {!!value ? (
+        <>
+          {' '}
+          <Input.Group className={styles.recurrenceInputGroup}>
+            <span>every </span>
             <InputNumber
               inputMode="numeric"
-              onChange={onCountChange}
+              onChange={onIntervalChange}
               pattern="[0-9]*"
-              placeholder="10"
+              placeholder="4"
               type="number"
-              value={existingCount}
+              value={existingInterval}
             />
-            <span>{` time${
-              existingCount && existingCount > 1 ? 's' : ''
+            <span>{` week${
+              existingInterval && existingInterval > 1 ? 's' : ''
             }`}</span>
-          </>
-        ) : null}
-      </Input.Group>
 
-      <Checkbox checked={!existingCount} onChange={onInfiniteChange}>
-        Repeats indefinitely?
-      </Checkbox>
+            {existingCount ? (
+              <>
+                <span> for </span>
+                <InputNumber
+                  inputMode="numeric"
+                  onChange={onCountChange}
+                  pattern="[0-9]*"
+                  placeholder="10"
+                  type="number"
+                  value={existingCount}
+                />
+                <span>{` time${
+                  existingCount && existingCount > 1 ? 's' : ''
+                }`}</span>
+              </>
+            ) : null}
+          </Input.Group>
+          <Checkbox checked={!existingCount} onChange={onInfiniteChange}>
+            Repeats indefinitely?
+          </Checkbox>{' '}
+        </>
+      ) : null}
     </>
   )
 
