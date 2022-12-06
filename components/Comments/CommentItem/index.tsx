83c1a4d6bcc44d1cff536ac8675cab5a81463b59
David Ruisinger <david.ruisinger@gmail.com>
2022-12-06 12:25:30 +0100

    feat: reactions on actionComments

--- components/Comments/CommentItem/index.tsx
+++ components/Comments/CommentItem/index.tsx
@@ -1,16 +1,20 @@
-import {
+import Icon, {
   DeleteOutlined,
   EditOutlined,
   SettingOutlined,
 } from '@ant-design/icons'
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
 import { Button, Popconfirm, Popover, Space } from 'antd'
 import classNames from 'classnames'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useUser } from '../../../hooks/user'
+import AddReactionIcon from '../../../public/img/icons/add-reaction.svg'
 import {
   ActionCommentFragment,
   useDeleteActionCommentMutation,
+  useReactOnActionCommentMutation,
 } from '../../../services/lfca-backend'
 import {
   DEFAULT_FONT_SIZE,
@@ -32,7 +36,9 @@ interface CommentItemProps {
 const VISIBLE_ROWS = 7
 
 export const CommentItem = ({ comment, isChild, onEdit }: CommentItemProps) => {
+  const [emojiPickerOpen, setEmojiPickerOpen] = useState(false)
   const [, deleteActionComment] = useDeleteActionCommentMutation()
+  const [, reactOnActionComment] = useReactOnActionCommentMutation()
 
   const onDelete = async () => {
     await deleteActionComment({
@@ -42,63 +48,120 @@ export const CommentItem = ({ comment, isChild, onEdit }: CommentItemProps) => {
     })
   }
 
-  const { isAdmin } = useUser()
+  const onReact = async (reaction: string, remove?: boolean) => {
+    await reactOnActionComment({
+      input: {
+        actionCommentId: comment.id,
+        reaction,
+        remove,
+      },
+    })
+  }
+
+  const { isAdmin, user } = useUser()
 
   return (
-    <div className={styles.commentItem}>
-      {isChild ? (
-        <CommentAuthor
-          author={comment.author ?? undefined}
-          className={styles.childCommentAuthor}
-        />
-      ) : null}
-      <div
-        className={classNames({
-          [styles.body]: true,
-          [styles.childComment]: isChild,
-        })}
-      >
-        <p className={styles.time}>{toReadibleDate(comment.createdAt)}</p>
+    <div className={styles.commentContainer}>
+      <div className={styles.commentContent}>
+        {isChild ? (
+          <CommentAuthor
+            author={comment.author ?? undefined}
+            className={styles.childCommentAuthor}
+          />
+        ) : null}
+        <div
+          className={classNames({
+            [styles.body]: true,
+            [styles.childComment]: isChild,
+          })}
+        >
+          <p className={styles.time}>{toReadibleDate(comment.createdAt)}</p>
 
-        {isAdmin && (
-          <Popover
-            className={styles.adminActions}
-            content={
-              <Space>
-                <Popconfirm
-                  cancelText="No"
-                  okText="Yes"
-                  onConfirm={onDelete}
-                  title="Are you sure to delete this comment?"
-                >
-                  <Button icon={<DeleteOutlined />} size="small" />
-                </Popconfirm>
-                <Button
-                  icon={<EditOutlined />}
-                  onClick={() => onEdit(comment as ActionCommentFragment)}
-                  size="small"
-                />
-              </Space>
-            }
-            placement="left"
-          >
-            <Button icon={<SettingOutlined />} size="small" />
-          </Popover>
-        )}
+          {isAdmin && (
+            <Popover
+              className={styles.adminActions}
+              content={
+                <Space>
+                  <Popconfirm
+                    cancelText="No"
+                    okText="Yes"
+                    onConfirm={onDelete}
+                    title="Are you sure to delete this comment?"
+                  >
+                    <Button icon={<DeleteOutlined />} size="small" />
+                  </Popconfirm>
+                  <Button
+                    icon={<EditOutlined />}
+                    onClick={() => onEdit(comment as ActionCommentFragment)}
+                    size="small"
+                  />
+                </Space>
+              }
+              placement="left"
+            >
+              <Button icon={<SettingOutlined />} size="small" />
+            </Popover>
+          )}
 
-        <ShowMore
-          blurColor="#f9f9f9"
-          buttonProps={{ size: 'small', type: 'link' }}
-          className={styles.showMoreContent}
-          maskMode="blur"
-          maxHeight={DEFAULT_FONT_SIZE * DEFAULT_LINE_HEIGHT * VISIBLE_ROWS}
-          text={<MarkdownContent content={comment.message} />}
-        />
-        <div className={styles.attachments}>
-          {comment.attachments?.map((attachment, i) => (
-            <AttachmentButton attachment={attachment} key={`att-${i}`} />
-          ))}
+          <ShowMore
+            blurColor="#f9f9f9"
+            buttonProps={{ size: 'small', type: 'link' }}
+            className={styles.showMoreContent}
+            maskMode="blur"
+            maxHeight={DEFAULT_FONT_SIZE * DEFAULT_LINE_HEIGHT * VISIBLE_ROWS}
+            text={<MarkdownContent content={comment.message} />}
+          />
+          <div className={styles.attachments}>
+            {comment.attachments?.map((attachment, i) => (
+              <AttachmentButton attachment={attachment} key={`att-${i}`} />
+            ))}
+          </div>
         </div>
+      </div>
+      <div className={styles.commentReactions}>
+        {Object.keys(comment.reactions as Record<string, string[]>).map(
+          (emoji) => {
+            const reactedUserIds: string[] = comment.reactions[emoji]
+            const isActive = !!user?.id && reactedUserIds.includes(user.id)
+
+            return (
+              <Button
+                className={styles.reactionButton}
+                key={emoji}
+                onClick={() => onReact(emoji, isActive)}
+                shape="round"
+                size="small"
+                type={isActive ? 'primary' : 'ghost'}
+              >
+                {`${emoji} ${reactedUserIds.length}`}
+              </Button>
+            )
+          }
+        )}
+        <Popover
+          content={
+            <Picker
+              data={data}
+              onEmojiSelect={({ native }: { native: string }) => {
+                onReact(native)
+                setEmojiPickerOpen(false)
+              }}
+              theme="light"
+            />
+          }
+          destroyTooltipOnHide={true}
+          onOpenChange={setEmojiPickerOpen}
+          open={emojiPickerOpen}
+          overlayClassName={styles.emojiPickerPopover}
+          trigger="click"
+        >
+          <Button
+            icon={<Icon component={AddReactionIcon} />}
+            shape="round"
+            size="small"
+            type="text"
+          />
+        </Popover>
       </div>
     </div>
   )
