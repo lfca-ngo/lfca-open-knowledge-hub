ee4b1dbe9a9a34e41b0736aa1540258c7cc89d95
David Ruisinger <david.ruisinger@gmail.com>
2022-12-12 12:33:13 +0100

    feat: hard-code google redirect url

--- pages/api/add-google-calendar-event.ts
+++ pages/api/add-google-calendar-event.ts
@@ -8,7 +8,7 @@ import { createHtmlDescription } from '../../utils/generate-calendar-links'
 const oauth2Client = new google.auth.OAuth2(
   process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.NEXT_PUBLIC_URL
+  'https://app.lfca.earth'
 )
 
 const calendar = google.calendar('v3')
--- pages/api/get-google-calendar-list.ts
+++ pages/api/get-google-calendar-list.ts
@@ -7,7 +7,7 @@ import { getErrorMessage } from '../../utils'
 const oauth2Client = new google.auth.OAuth2(
   process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.NEXT_PUBLIC_URL
+  'https://app.lfca.earth'
 )
 
 const calendar = google.calendar('v3')
