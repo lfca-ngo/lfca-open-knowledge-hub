5dac3fedf4ba1d147dbe99f93bb6ee7f42dc6ca4
David Ruisinger <david.ruisinger@gmail.com>
2022-12-23 13:33:40 +0100

    chore: cleanup (#134)

--- types/global.ts
+++ types/global.ts
@@ -6,10 +6,7 @@ declare global {
       CF_PREVIEW_ACCESS_TOKEN: string
       CF_SPACE_ID: string
       DISABLE_CONTENTFUL_SERVICE_CACHE: string
-      GOOGLE_CLIENT_SECRET: string
-      JWT_SHARE_TOKEN_PRIVATE_KEY: string
       MAINTENANCE: string
-      NEXT_PUBLIC_GOOGLE_CLIENT_ID: string
       NEXT_PUBLIC_URL: string
     }
   }
